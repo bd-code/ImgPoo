47be63ff82a36a2729ead3c75e549801a39e62dc
Bob <nomail>
2020-04-11 20:50:32 -0400

    Initial code commit.

---
+++ Source/ImgPoo/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace ImgPoo {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imgBox = new System.Windows.Forms.PictureBox();
+            this.menuBar = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetZoomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toggleStatusBarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.filePathStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sizeStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.zoomStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.imgPanel = new System.Windows.Forms.Panel();
+            this.changeBGColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).BeginInit();
+            this.menuBar.SuspendLayout();
+            this.statusBar.SuspendLayout();
+            this.imgPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imgBox
+            // 
+            this.imgBox.Location = new System.Drawing.Point(0, 0);
+            this.imgBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.imgBox.Name = "imgBox";
+            this.imgBox.Size = new System.Drawing.Size(40, 40);
+            this.imgBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgBox.TabIndex = 0;
+            this.imgBox.TabStop = false;
+            this.imgBox.Paint += new System.Windows.Forms.PaintEventHandler(this.imgBox_Paint);
+            // 
+            // menuBar
+            // 
+            this.menuBar.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuBar.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuBar.Location = new System.Drawing.Point(0, 0);
+            this.menuBar.Name = "menuBar";
+            this.menuBar.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuBar.Size = new System.Drawing.Size(980, 28);
+            this.menuBar.TabIndex = 1;
+            this.menuBar.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openImageToolStripMenuItem,
+            this.openDirectoryToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openImageToolStripMenuItem
+            // 
+            this.openImageToolStripMenuItem.Name = "openImageToolStripMenuItem";
+            this.openImageToolStripMenuItem.Size = new System.Drawing.Size(185, 26);
+            this.openImageToolStripMenuItem.Text = "Open Image";
+            this.openImageToolStripMenuItem.Click += new System.EventHandler(this.openImageToolStripMenuItem_Click);
+            // 
+            // openDirectoryToolStripMenuItem
+            // 
+            this.openDirectoryToolStripMenuItem.Name = "openDirectoryToolStripMenuItem";
+            this.openDirectoryToolStripMenuItem.Size = new System.Drawing.Size(185, 26);
+            this.openDirectoryToolStripMenuItem.Text = "Open Directory";
+            this.openDirectoryToolStripMenuItem.Click += new System.EventHandler(this.openDirectoryToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zoomInToolStripMenuItem,
+            this.zoomOutToolStripMenuItem,
+            this.resetZoomToolStripMenuItem,
+            this.changeBGColorToolStripMenuItem,
+            this.toggleStatusBarToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.viewToolStripMenuItem.Text = "&View";
+            // 
+            // zoomInToolStripMenuItem
+            // 
+            this.zoomInToolStripMenuItem.Name = "zoomInToolStripMenuItem";
+            this.zoomInToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.zoomInToolStripMenuItem.Text = "Zoom In";
+            this.zoomInToolStripMenuItem.Click += new System.EventHandler(this.zoomInToolStripMenuItem_Click);
+            // 
+            // zoomOutToolStripMenuItem
+            // 
+            this.zoomOutToolStripMenuItem.Name = "zoomOutToolStripMenuItem";
+            this.zoomOutToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.zoomOutToolStripMenuItem.Text = "Zoom Out";
+            this.zoomOutToolStripMenuItem.Click += new System.EventHandler(this.zoomOutToolStripMenuItem_Click);
+            // 
+            // resetZoomToolStripMenuItem
+            // 
+            this.resetZoomToolStripMenuItem.Name = "resetZoomToolStripMenuItem";
+            this.resetZoomToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.resetZoomToolStripMenuItem.Text = "Reset Zoom";
+            this.resetZoomToolStripMenuItem.Click += new System.EventHandler(this.resetZoomToolStripMenuItem_Click);
+            // 
+            // toggleStatusBarToolStripMenuItem
+            // 
+            this.toggleStatusBarToolStripMenuItem.Name = "toggleStatusBarToolStripMenuItem";
+            this.toggleStatusBarToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.toggleStatusBarToolStripMenuItem.Text = "Toggle Status Bar";
+            this.toggleStatusBarToolStripMenuItem.Click += new System.EventHandler(this.toggleStatusBarToolStripMenuItem_Click);
+            // 
+            // statusBar
+            // 
+            this.statusBar.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filePathStatusLabel,
+            this.sizeStatusLabel,
+            this.zoomStatusLabel});
+            this.statusBar.Location = new System.Drawing.Point(0, 1026);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Padding = new System.Windows.Forms.Padding(1, 0, 18, 0);
+            this.statusBar.Size = new System.Drawing.Size(980, 29);
+            this.statusBar.TabIndex = 2;
+            this.statusBar.Text = "statusStrip1";
+            // 
+            // filePathStatusLabel
+            // 
+            this.filePathStatusLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.filePathStatusLabel.Name = "filePathStatusLabel";
+            this.filePathStatusLabel.Size = new System.Drawing.Size(846, 24);
+            this.filePathStatusLabel.Spring = true;
+            this.filePathStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // sizeStatusLabel
+            // 
+            this.sizeStatusLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.sizeStatusLabel.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.sizeStatusLabel.Name = "sizeStatusLabel";
+            this.sizeStatusLabel.Size = new System.Drawing.Size(55, 24);
+            this.sizeStatusLabel.Text = "Size:   ";
+            this.sizeStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // zoomStatusLabel
+            // 
+            this.zoomStatusLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.zoomStatusLabel.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.zoomStatusLabel.Name = "zoomStatusLabel";
+            this.zoomStatusLabel.Size = new System.Drawing.Size(60, 24);
+            this.zoomStatusLabel.Text = "Zoom: ";
+            this.zoomStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // imgPanel
+            // 
+            this.imgPanel.AutoScroll = true;
+            this.imgPanel.BackColor = System.Drawing.Color.Black;
+            this.imgPanel.Controls.Add(this.imgBox);
+            this.imgPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imgPanel.Location = new System.Drawing.Point(0, 28);
+            this.imgPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.imgPanel.Name = "imgPanel";
+            this.imgPanel.Size = new System.Drawing.Size(980, 998);
+            this.imgPanel.TabIndex = 3;
+            // 
+            // changeBGColorToolStripMenuItem
+            // 
+            this.changeBGColorToolStripMenuItem.Name = "changeBGColorToolStripMenuItem";
+            this.changeBGColorToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.changeBGColorToolStripMenuItem.Text = "Change BG Color";
+            this.changeBGColorToolStripMenuItem.Click += new System.EventHandler(this.changeBGColorToolStripMenuItem_Click);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.FullOpen = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(980, 1055);
+            this.Controls.Add(this.imgPanel);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.menuBar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuBar;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ImgPoo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).EndInit();
+            this.menuBar.ResumeLayout(false);
+            this.menuBar.PerformLayout();
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
+            this.imgPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgBox;
+        private System.Windows.Forms.MenuStrip menuBar;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openDirectoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomOutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toggleStatusBarToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusBar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ToolStripStatusLabel filePathStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel zoomStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel sizeStatusLabel;
+        private System.Windows.Forms.Panel imgPanel;
+        private System.Windows.Forms.ToolStripMenuItem resetZoomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeBGColorToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Source/ImgPoo/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ImgPoo {
+    public partial class Form1 : Form {
+
+        private Bitmap _img;
+        private List<string> _imagefiles;
+        private int _dirIndex;
+
+        private int _zoomLevel;
+        private bool _imgChanged;
+
+        public Form1() {
+            _imagefiles = new List<string>();
+            _dirIndex = 0;
+
+            _zoomLevel = 1;
+            _imgChanged = false;
+
+            InitializeComponent();
+
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            folderBrowserDialog1.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            if (Environment.GetCommandLineArgs().Length > 1) {
+                string imgpath = Environment.GetCommandLineArgs()[1];
+                if (isValidImage(imgpath)) {
+                    string path = Path.GetDirectoryName(imgpath);
+                    getFiles(path);
+                    _dirIndex = _imagefiles.IndexOf(imgpath);
+                    changeImage(imgpath);
+                }
+            }
+
+            zoomStatusLabel.Text = "Zoom: " + _zoomLevel.ToString();
+        }
+
+        private void openImageToolStripMenuItem_Click(object sender, EventArgs e) {
+            openFileDialog1.ShowDialog();
+            if (File.Exists(openFileDialog1.FileName)) {
+                string path = Path.GetDirectoryName(openFileDialog1.FileName);
+                openFileDialog1.InitialDirectory = path;
+                getFiles(path);
+
+                if (isValidImage(openFileDialog1.FileName)) {
+                    _dirIndex = _imagefiles.IndexOf(openFileDialog1.FileName);
+                    changeImage(openFileDialog1.FileName);
+                }
+
+            }
+        }
+
+        private void openDirectoryToolStripMenuItem_Click(object sender, EventArgs e) {
+            folderBrowserDialog1.ShowDialog();
+            if (Directory.Exists(folderBrowserDialog1.SelectedPath)) {
+                getFiles(folderBrowserDialog1.SelectedPath);
+
+                if (_imagefiles.Count > 0 && isValidImage(_imagefiles[0])) {
+                    _dirIndex = 0;
+                    changeImage(_imagefiles[0]);
+                }
+            }
+        }
+
+        private void zoomInToolStripMenuItem_Click(object sender, EventArgs e) {
+            zoomIn();
+        }
+
+        private void zoomOutToolStripMenuItem_Click(object sender, EventArgs e) {
+            zoomOut();
+        }
+
+        private void resetZoomToolStripMenuItem_Click(object sender, EventArgs e) {
+            zoomReset();
+        }
+
+        private void toggleStatusBarToolStripMenuItem_Click(object sender, EventArgs e) {
+            statusBar.Visible = !statusBar.Visible;
+        }
+
+        private void imgBox_Paint(object sender, PaintEventArgs e) {
+            e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
+
+            if (_imgChanged) {
+                resetImgBox();
+                _imgChanged = false;
+            }
+
+            if (_img is Bitmap) {
+                Rectangle rect = new Rectangle(
+                    0,
+                    0,
+                    imgBox.Width,
+                    imgBox.Height);
+                e.Graphics.DrawImage(_img, rect);
+
+                menuBar.BringToFront();
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e) {
+            resetImgBox();
+            imgBox.Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Up) {
+                zoomIn();
+            }
+            else if (e.KeyCode == Keys.Down) {
+                zoomOut();
+            }
+            else if (e.KeyCode == Keys.Right) {
+                nextImage();
+            }
+            else if (e.KeyCode == Keys.Left) {
+                prevImage();
+            }
+        }
+
+        //*////////////////////////////////////////////////////////////////////
+
+        private void getFiles(string dir) {
+            var exts = new List<string> { ".bmp", ".gif", ".jpg", ".jpeg", ".png" };
+            _imagefiles = new List<string>(
+                Directory.EnumerateFiles(dir)
+                .Where(s => exts.Contains(Path.GetExtension(s).ToLowerInvariant()))
+                );
+        }
+
+        private void changeImage(string filename) {
+            _img = new Bitmap(filename);
+            filePathStatusLabel.Text = filename;
+            sizeStatusLabel.Text = "Size: " + _img.Width + " x " + _img.Height + "";
+            _zoomLevel = 1;
+            zoomStatusLabel.Text = "Zoom: " + _zoomLevel.ToString();
+            _imgChanged = true;
+            imgBox.Refresh();
+        }
+
+        private bool isValidImage(string filename) {
+            try {
+                Image.FromFile(filename);
+            }
+            catch (OutOfMemoryException ex) {
+                return false;
+            }
+            return true;
+        }
+
+        private void nextImage() {
+            if (_imagefiles.Count() > 0) {
+                _dirIndex = ++_dirIndex % _imagefiles.Count();
+                changeImage(_imagefiles[_dirIndex]);
+            }
+        }
+
+        private void prevImage() {
+            if (_imagefiles.Count() > 0) {
+                _dirIndex = (_dirIndex == 0) ? _imagefiles.Count() - 1 : --_dirIndex;
+                changeImage(_imagefiles[_dirIndex]);
+            }
+        }
+
+        private void zoomIn() {
+            _zoomLevel = (_zoomLevel < 8) ? _zoomLevel+1 : _zoomLevel;
+            zoomStatusLabel.Text = "Zoom: " + _zoomLevel.ToString();
+            resetImgBox();
+            imgBox.Refresh();
+        }
+
+        private void zoomOut() {
+            _zoomLevel = (_zoomLevel > 1) ? _zoomLevel-1 : _zoomLevel;
+            zoomStatusLabel.Text = "Zoom: " + _zoomLevel.ToString();
+            resetImgBox();
+            imgBox.Refresh();
+        }
+
+        private void zoomReset() {
+            _zoomLevel = 1;
+            zoomStatusLabel.Text = "Zoom: " + _zoomLevel.ToString();
+            resetImgBox();
+            imgBox.Refresh();
+        }
+
+        private void resetImgBox() {
+            if (_img is Bitmap) {
+                int imgWidth = _img.Width * _zoomLevel;
+                int imgHeight = _img.Height * _zoomLevel;
+
+                imgBox.Location = new Point(
+                    Math.Max((Width - imgWidth) / 2, 0),
+                    Math.Max((Height - imgHeight) / 2, 0 + menuBar.Size.Height)
+                    );
+                imgBox.Width = imgWidth;
+                imgBox.Height = imgHeight;
+            }
+        }
+
+        private void changeBGColorToolStripMenuItem_Click(object sender, EventArgs e) {
+            colorDialog1.ShowDialog();
+            imgPanel.BackColor = colorDialog1.Color;
+        }
+    }
+}
